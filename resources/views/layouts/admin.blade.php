58b286b8df048738f020168d4d0a9c6fc19f88b4
mehedy hassan <mehedy.hassan@northsouth.edu>
2020-06-12 20:42:25 +0600

    image upload in form completed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,6 +14,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
 </head>
 <body>
 
@@ -24,9 +26,9 @@
         </div>
         <ul class="nav navbar-nav">
           <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">Page 1</a></li>
-          <li><a href="#">Page 2</a></li>
-          <li><a href="#">Page 3</a></li>
+          <li><a href="{{route('admin.users.index')}}">All Users</a></li>
+          <li><a href="{{route('admin.users.create')}}">Create</a></li>
+          <li><a href="#">Update</a></li>
         </ul>
       </div>
     </nav>
